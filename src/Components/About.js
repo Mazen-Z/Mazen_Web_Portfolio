3a608adaecf1b942fccc6718508fa1b0dde03634
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2024-01-12 01:19:18 -0500

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -52,7 +52,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a onClick={handleDownloadClick} className="button">
+                <a href={resumeDownload} className="button">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
